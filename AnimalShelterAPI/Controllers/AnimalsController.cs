2c3383655f0bc1eabb84900021e7c65d61a3cf06
Joseph Giunta <jgnohack@gmail.com>
2021-01-30 09:45:20 -0800

    update AnimalsController

--- AnimalShelterAPI/Controllers/AnimalsController.cs
+++ AnimalShelterAPI/Controllers/AnimalsController.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using AnimalShelterApi.Models;
 
 namespace AnimalShelterAPI.Controllers
 {
