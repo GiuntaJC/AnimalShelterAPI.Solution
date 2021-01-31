5fd30e3066375965c49b8051206fb0f374a0b85e
Joseph Giunta <jgnohack@gmail.com>
2021-01-30 16:54:34 -0800

    make name required

--- AnimalShelterAPI/Models/Animal.cs
+++ AnimalShelterAPI/Models/Animal.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AnimalShelterApi.Models
 {
 	public class Animal {
 		public int AnimalId { get; set; }
 		public string Name { get; set; }
+    [Required]
     public string Species { get; set; }
     public int Age { get; set; }
     public string Gender { get; set; }
